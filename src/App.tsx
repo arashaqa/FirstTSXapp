9308cf2e113e4529ef339ecbff1599640d26d9a8
arashaqa <balali.arash@gmail.com>
2021-10-21 11:26:20 +0330

    v1

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className="row">
+        <div className="col">
+          header
+        </div>
+        <div className="row">
+          <div className="col">
+            category
+          </div>
+          <div>
+            product
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            category
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+      
+  </div>
+  );
+}
+
+export default App;
